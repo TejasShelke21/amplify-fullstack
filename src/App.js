d772d80bb40494df70541d3bb27af5a7e1dc90fc
TejasShelke21 <141266499+TejasShelke21@users.noreply.github.com>
2024-04-24 20:15:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// Put your code below this line.
+
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
@@ -9,14 +11,15 @@ import {
   TextField,
   View,
   withAuthenticator,
-  Image,
 } from "@aws-amplify/ui-react";
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
-import { API, Storage } from "aws-amplify";
+import { generateClient } from "aws-amplify/api";
+
+const client = generateClient();
 
 const App = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
@@ -26,31 +29,19 @@ const App = ({ signOut }) => {
   }, []);
 
   async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
+    const apiData = await client.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const url = await Storage.get(note.name);
-          note.image = url;
-        }
-        return note;
-      })
-    );
     setNotes(notesFromAPI);
   }
 
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
-    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
-      image: image.name,
     };
-    if (!!data.image) await Storage.put(data.name, image);
-    await API.graphql({
+    await client.graphql({
       query: createNoteMutation,
       variables: { input: data },
     });
@@ -58,11 +49,10 @@ const App = ({ signOut }) => {
     event.target.reset();
   }
 
-  async function deleteNote({ id, name }) {
+  async function deleteNote({ id }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
-    await Storage.remove(name);
-    await API.graphql({
+    await client.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
     });
@@ -70,12 +60,12 @@ const App = ({ signOut }) => {
 
   return (
     <View className="App">
-      <Heading level={1}>My Notes App</Heading>
+      <Heading level={1}>My To-Do App</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
           <TextField
             name="name"
-            placeholder="Note Name"
+            placeholder="To-Do Name"
             label="Note Name"
             labelHidden
             variation="quiet"
@@ -83,24 +73,18 @@ const App = ({ signOut }) => {
           />
           <TextField
             name="description"
-            placeholder="Note Description"
+            placeholder="To-Do Description"
             label="Note Description"
             labelHidden
             variation="quiet"
             required
           />
-          <View
-            name="image"
-            as="input"
-            type="file"
-            style={{ alignSelf: "end" }}
-          />
           <Button type="submit" variation="primary">
-            Create Note
+            Create To-Do
           </Button>
         </Flex>
       </View>
-      <Heading level={2}>Current Notes</Heading>
+      <Heading level={2}>Current To-Do's</Heading>
       <View margin="3rem 0">
         {notes.map((note) => (
           <Flex
@@ -113,15 +97,8 @@ const App = ({ signOut }) => {
               {note.name}
             </Text>
             <Text as="span">{note.description}</Text>
-            {note.image && (
-              <Image
-                src={note.image}
-                alt={`visual aid for ${note.name}`}
-                style={{ width: 400 }}
-              />
-            )}
             <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
+              Delete To-Do
             </Button>
           </Flex>
         ))}
@@ -132,3 +109,4 @@ const App = ({ signOut }) => {
 };
 
 export default withAuthenticator(App);
+
